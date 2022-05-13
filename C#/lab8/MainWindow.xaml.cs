89f4c47ec4881999595ba2bd0d9d7d3561a92f32
bzylwis <nil112101@gmail.com>
2022-05-13 21:25:06 +0200

    finished lab 8

--- C#/lab8/MainWindow.xaml.cs
+++ C#/lab8/MainWindow.xaml.cs
@@ -22,9 +22,11 @@ namespace lab8_WPF_APP
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string rootPath = "";
         public MainWindow()
         {
             InitializeComponent();
+            
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
@@ -39,9 +41,11 @@ namespace lab8_WPF_APP
 
             if (resoult == System.Windows.Forms.DialogResult.OK)
             {
+                rootPath = dlg.SelectedPath;
                 treeView.Items.Clear();
                 treeView.Items.Add(LoadFiles(dlg.SelectedPath));
-                
+                //treeView.MouseDown += (sender, args) => treeView.SelectedItem = args.Node;
+
             }
 
         }
@@ -56,22 +60,21 @@ namespace lab8_WPF_APP
                 {
                     Header = dirInfo.Name,
                     Tag = path,
-                    //IsExpanded = true,
                     ContextMenu = CreateContextMenuDir()
                 };
+                if (rootPath == path)
+                {
+                    element.IsExpanded = true;
+                }
 
                 foreach (var fileInfo in dirInfo.GetFiles())
                 {
                     element.Items.Add(LoadFiles(fileInfo.FullName));
-
                 }
                 foreach (var subDirInfo in dirInfo.GetDirectories())
                 {
                     element.Items.Add(LoadFiles(subDirInfo.FullName));
-
                 }
-
-                
                 return element;
 
             } 
@@ -93,8 +96,9 @@ namespace lab8_WPF_APP
         private System.Windows.Controls.ContextMenu CreateContextMenuDir()
         {
             System.Windows.Controls.ContextMenu cm = new System.Windows.Controls.ContextMenu();
-            cm.Items.Add(new System.Windows.Controls.MenuItem() { Header = "Create" });
-            //cm.Items.Add(new System.Windows.Controls.MenuItem() { Header = "Delete" });
+            var btn1 = new System.Windows.Controls.MenuItem() { Header = "Create" };
+            btn1.Click += new RoutedEventHandler(CreateHandle);
+            cm.Items.Add(btn1);
             var btn2 = new System.Windows.Controls.MenuItem() { Header = "Delete" };
             btn2.Click += new RoutedEventHandler(DeleteHandle);
             cm.Items.Add(btn2);
@@ -106,7 +110,7 @@ namespace lab8_WPF_APP
             System.Windows.Controls.ContextMenu cm = new System.Windows.Controls.ContextMenu();
 
             var btn1 = new System.Windows.Controls.MenuItem() { Header = "Open" };
-            btn1.Click += new RoutedEventHandler(OpenFile);
+            btn1.Click += new RoutedEventHandler(OpenHandle);
 
             cm.Items.Add(btn1);
             var btn2 = new System.Windows.Controls.MenuItem() { Header = "Delete" };
@@ -116,7 +120,7 @@ namespace lab8_WPF_APP
 
             return cm;
         }
-        private void OpenFile(object sender, RoutedEventArgs e)
+        private void OpenHandle(object sender, RoutedEventArgs e)
         {
             TreeViewItem item = (TreeViewItem)treeView.SelectedItem;
             string fileData = File.ReadAllText((string)item.Tag);
@@ -125,20 +129,73 @@ namespace lab8_WPF_APP
         private void DeleteHandle(object sender, RoutedEventArgs e)
         {
             TreeViewItem item = (TreeViewItem)treeView.SelectedItem;
-            string path = (string)item.Tag;
-            DeleteElements(path);
+            if (item != null)
+            {
+                string path = (string)item.Tag;
+                DeleteElements(path);
+                ReloadFilesView();
+            }
 
+        }
+        private void CreateHandle(object sender, RoutedEventArgs e)
+        {
+            NewElementDiagol newElementDiagol = new NewElementDiagol();
+            var resoult = newElementDiagol.ShowDialog();
+
+            if (resoult == true)
+            {
+                TreeViewItem item = (TreeViewItem)treeView.SelectedItem;
+                string currPath = (string)item.Tag;
+                string newFilePath = currPath + "\\" + newElementDiagol.newFileName;
+
+                // if creating file ; 0 - directory
+                if (newElementDiagol.fileInfo[0] == 1)
+                {
+                    File.Create(newFilePath);
+                }
+                else
+                {
+                    Directory.CreateDirectory(newFilePath);
+                }
+                // setting attributes
+                FileAttributes attributes = FileAttributes.Normal;
+                if (newElementDiagol.fileInfo[1] == 1)
+                {
+                    attributes |= FileAttributes.ReadOnly;
+                }
+                if (newElementDiagol.fileInfo[2] == 1)
+                {
+                    attributes |= FileAttributes.Archive;
+                }
+                if (newElementDiagol.fileInfo[3] == 1)
+                {
+                    attributes |= FileAttributes.Hidden;
+                }
+                if (newElementDiagol.fileInfo[4] == 1)
+                {
+                    attributes |= FileAttributes.System;
+                }
+                File.SetAttributes(newFilePath, attributes);
+                ReloadFilesView();
+            }
         }
 
         private void DeleteElements(string path)
         {
+            FileAttributes attributes = File.GetAttributes(path);
+            File.SetAttributes(path, attributes & ~FileAttributes.ReadOnly);
+
             if (Directory.Exists(path))
             {
                 foreach(string element in Directory.GetFiles(path))
                 {
+                    FileAttributes attributes2 = File.GetAttributes(element);
+                    File.SetAttributes(element, attributes2 & ~FileAttributes.ReadOnly);
                     File.Delete(element);
                 }
                 foreach(string element in Directory.GetDirectories(path)) {
+                    FileAttributes attributes2 = File.GetAttributes(element);
+                    File.SetAttributes(element, attributes2 & ~FileAttributes.ReadOnly);
                     DeleteElements(element);
                 }
                 Directory.Delete(path);
@@ -147,6 +204,64 @@ namespace lab8_WPF_APP
             {
                 File.Delete(path);
             }
+        }
+
+        private void ExitProgram(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void ReloadFilesView()
+        {
+            if (rootPath != "")
+            {
+                treeView.Items.Clear();
+                treeView.Items.Add(LoadFiles(rootPath));
+
+            }
+        }
+       
+        private void FileViewer_Scroll(object sender, System.Windows.Controls.Primitives.ScrollEventArgs e)
+        {
+
+        }
+        private void SelectedItenChangeHandle(object sender, RoutedPropertyChangedEventArgs<Object> e)
+        {
+            TreeViewItem item = (TreeViewItem)treeView.SelectedItem;
+            if (item == null)
+                return;
+
+            string currPath = (string)item.Tag;
+            FileAttributes attributes;
+            
+            if (File.Exists(currPath))
+            {
+                attributes = File.GetAttributes(currPath);
+            } 
+            else
+            {
+                DirectoryInfo dir = new DirectoryInfo(currPath);
+                attributes = dir.Attributes;
+            }
+
+            //attributes.ToString();
+            string rash = "";
+            if (attributes.HasFlag(FileAttributes.ReadOnly))
+                rash += "r";
+            else
+                rash += "-";
+            if (attributes.HasFlag(FileAttributes.Archive))
+                rash += "a";
+            else
+                rash += "-";
+            if (attributes.HasFlag(FileAttributes.System))
+                rash += "s";
+            else
+                rash += "-";
+            if (attributes.HasFlag(FileAttributes.Hidden))
+                rash += "h";
+            else
+                rash += "-";
+            rashInfo.Text = rash;
         }
     }
 }
---
+++ C#/lab8/Window1.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace lab8_WPF_APP
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class NewElementDiagol : Window
+    {
+        public string newFileName { get; set; }
+        public byte[] fileInfo { get; set; } = new byte[5];
+        public NewElementDiagol()
+        {
+            InitializeComponent();
+        }
+
+        private void okButtonClick(object sender, RoutedEventArgs e)
+        {
+            string input = inputBox.Text;
+            if ((!Regex.IsMatch(input, "^[a-zA-Z0-9_~-]{1,8}\\.(txt|php|html)$") && fileRadio.IsChecked == true)||input =="")
+            {
+                badNameBorder.Visibility = Visibility.Visible;
+                wrongNameInfo.Visibility = Visibility.Visible;
+                return;
+            }
+            
+            this.newFileName = input;
+            
+            if (fileRadio.IsChecked == true)
+            {
+                fileInfo[0] = 1;
+            }
+            if (readOnlyCheckBox.IsChecked == true)
+            {
+                fileInfo[1] = 1;
+            }
+            if (archiveCheckBox.IsChecked == true)
+            {
+                fileInfo[2] = 1;
+            }
+            if (hiddenCheckBox.IsChecked == true)
+            {
+                fileInfo[3] = 1;
+            }
+            if (systemCheckBox.IsChecked == true)
+            {
+                fileInfo[4] = 1;
+            }
+            this.DialogResult = true;
+        }
+    }
+}
